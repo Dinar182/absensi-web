99aad4f726bfe0a56d5e46b6d96001fccead9099
thinkerdust <56123956+thinkerdust@users.noreply.github.com>
2023-05-07 20:28:10 +0700

    Update Api.php

--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -4,7 +4,7 @@ class Api extends CI_Controller
 {
     function __construct()
     {
-		header("Access-Control-Allow-Origin: *"); 
+	header("Access-Control-Allow-Origin: *"); 
         header("Access-Control-Allow-Methods: POST, OPTIONS"); 
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method"); 
         header("Access-Control-Max-Age: 86400");
